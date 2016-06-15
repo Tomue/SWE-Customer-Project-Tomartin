7cc8b6fa296d34ce66f8d8d11e30ee239d09d5b6
MarKogler <S1510564009@students.fh-wels.at>
2016-06-15 16:01:03 +0200

    Updated Comments
    Comments in Class Customer are updated

--- SWE_Project/FrmMain_LoginForm_PartPachler/Customer.cs
+++ SWE_Project/FrmMain_LoginForm_PartPachler/Customer.cs
@@ -9,7 +9,7 @@ namespace FrmMain_LoginForm_PartPachler
     public class Customer
     {
         #region Membervariables, Variables and Constants
-        private static int RunningCustomerNumber = 0;
+        private static int runningCustomerNumber = 0;
 
         private int customerNumber;
         private string firstName;
@@ -20,31 +20,48 @@ namespace FrmMain_LoginForm_PartPachler
         #endregion
 
         #region Constructor
+        /// <summary>
+        /// Default Constructor which is used to create a Customer Object which is not already in the database.
+        /// The date of the last change is initialized to Datetime.Now and balancing is initialized to 0.0 which are the default values.
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="eMailAdress"></param>
         public Customer(string firstName,
                         string lastName,
                         string eMailAdress)
         {
-            this.CustomerNumber = RunningCustomerNumber;
-            RunningCustomerNumber++;
+            this.CustomerNumber = runningCustomerNumber;
+            runningCustomerNumber++;
             this.FirstName = firstName;
             this.LastName = lastName;
             this.EMailAdress = eMailAdress;
             this.Balancing = 0.0;
             this.DateLastChange = DateTime.Now;
         }
+
+        /// <summary>
+        /// Special Constructor to create a Customer Object out of already existing data.
+        /// The date of date last change and the balancing are not initialized by default values.
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="eMailAdress"></param>
+        /// <param name="dateLastChanged"></param>
+        /// <param name="balance"></param>
         public Customer(string firstName,
                         string lastName,
                         string eMailAdress,
                         DateTime dateLastChanged,
                         double balance)
         {
-            this.CustomerNumber = RunningCustomerNumber;
-            RunningCustomerNumber++;
+            this.CustomerNumber = runningCustomerNumber;
+            runningCustomerNumber++;
             this.FirstName = firstName;
             this.LastName = lastName;
             this.EMailAdress = eMailAdress;
             this.Balancing = balance;
-            this.DateLastChange = dateLastChanged;
+            this.dateLastChange = dateLastChanged; //there is no actual date of the last change so it do not need to be checked by the property
         }
         #endregion
 
@@ -60,6 +77,11 @@ namespace FrmMain_LoginForm_PartPachler
                 this.customerNumber = value;
             }
         }
+
+        /// <summary>
+        /// This Property does not allow empty strings as input.
+        /// </summary>
+        /// <exception cref="ArgumentException">Exception is thrown if an empty string is written on the property.</exception>
         public string FirstName
         {
             get
@@ -78,6 +100,11 @@ namespace FrmMain_LoginForm_PartPachler
                 }
             }
         }
+
+        /// <summary>
+        /// This Property does not allow empty strings as input.
+        /// </summary>
+        /// <exception cref="ArgumentException">Exception is thrown if an empty string is written on the property.</exception>
         public string LastName
         {
             get
@@ -96,6 +123,11 @@ namespace FrmMain_LoginForm_PartPachler
                 }
             }
         }
+
+        /// <summary>
+        /// This Property does not allow empty strings as input.
+        /// </summary>
+        /// <exception cref="ArgumentException">Exception is thrown if an empty string is written on the property.</exception>
         public string EMailAdress
         {
             get
@@ -114,6 +146,10 @@ namespace FrmMain_LoginForm_PartPachler
                 }
             }
         }
+
+        /// <summary>
+        /// Changes in Balancing cause automatically an update of the date of the last change.
+        /// </summary>
         public double Balancing
         {
             get
@@ -126,6 +162,11 @@ namespace FrmMain_LoginForm_PartPachler
                 this.DateLastChange = DateTime.Now;
             }
         }
+
+        /// <summary>
+        /// It is not allowed to change the date of the last change to an older one
+        /// </summary>
+        /// <exception cref="ArgumentException">Exception is thrown if the date of the last change should be changed to an older one</exception>
         public DateTime DateLastChange
         {
             get
@@ -134,19 +175,31 @@ namespace FrmMain_LoginForm_PartPachler
             }
             set
             {
-                //if (DateTime.Compare(value, this.dateLastChange) >= 0)
+                if (DateTime.Compare(value, this.dateLastChange) >= 0)
                 {
                     this.dateLastChange = value;
                 }
-                //else
-                //{
-                //    throw new ArgumentException("Date is older than date of actual last change");
-                //}
+                else
+                {
+                    throw new ArgumentException("Date is older than date of actual last change");
+                }
             }
         }
         #endregion
 
         #region static Methods
+        /// <summary>
+        /// Validates the E-Mail Adress. There must be only one @ (Violation causes Errorcode -1). There must be at least one dot after the @.
+        /// (Violation causes Errorcode -2). The part after the final dot must be at a length between 2 and 4.(Violation causes Errorcode -3)
+        /// The final part must contain only letters. (Violation causes Errorcode -4). There must be at least one Character before the @.
+        /// (Violation causes Errorcode -5). There is no dot at the start, end of next to the @. (Violation causes Errorcode -6).
+        /// There must be no invalid Symbols. Valid Symbols are letters, digits and the following special symbols: 
+        /// ".","!","#","$","%","&","'","*","+","-","/","=","?","^"," ","`","{","|","}","~","_","@". (Violations causes Errorcode -7).
+        /// The E-Mail Adress must not be already listed in the system. (Violation causes Errorcode -8). A valid E-Mail Adress causes Errorcode 0.
+        /// </summary>
+        /// <param name="customerList"></param>
+        /// <param name="eMailAdress"></param>
+        /// <returns>Errorcode</returns>
         public static int ValidateEMailAdress(List<Customer> customerList, string eMailAdress)
         {
             int errorCode = 0;
@@ -216,7 +269,12 @@ namespace FrmMain_LoginForm_PartPachler
 
             return (errorCode);
         }
-        //Checks if eMailAdress contains exactly one @
+        
+        /// <summary>
+        /// Checks if eMailAdress contains exactly one @
+        /// </summary>
+        /// <param name="eMailAdress"></param>
+        /// <returns>true if there is only one @</returns>
         private static bool EMailAdressContainsExactlyOneAt(string eMailAdress)
         {
             bool result = true;
@@ -236,7 +294,11 @@ namespace FrmMain_LoginForm_PartPachler
             return (result);
         }
 
-        //Checks if there is a '.' in the part after the @
+        /// <summary>
+        /// Checks if there is a '.' in the part after the @
+        /// </summary>
+        /// <param name="eMailAdress"></param>
+        /// <returns>true if there is a dot after the @</returns>
         private static bool ContainsDotAfterAt(string eMailAdress)
         {
             bool result = true;
@@ -249,14 +311,22 @@ namespace FrmMain_LoginForm_PartPachler
             return (result);
         }
 
-        //Returns the number of characters after the last '.'
+        /// <summary>
+        /// Returns the number of characters after the last '.'
+        /// </summary>
+        /// <param name="eMailAdress"></param>
+        /// <returns>lenght of the final part</returns>
         private static int LengthOfFinalPart(string eMailAdress)
         {
             int lastindex = (eMailAdress.LastIndexOf('.') + 1);
             return (eMailAdress.Length - (lastindex));
         }
 
-        //Checks if there are only Letters in the Part after the last '.'
+        /// <summary>
+        /// Checks if there are only Letters in the Part after the last '.'
+        /// </summary>
+        /// <param name="eMailAdress"></param>
+        /// <returns>true if there are only letters in the final part</returns>
         private static bool FinalPartsContainsOnlyLetters(string eMailAdress)
         {
             bool result = true;
@@ -274,7 +344,11 @@ namespace FrmMain_LoginForm_PartPachler
             return (result);
         }
 
-        //Checks if there is a charakter before the @
+        /// <summary>
+        /// Checks if there is a charakter before the @
+        /// </summary>
+        /// <param name="eMailAdress"></param>
+        /// <returns>true if there is at least one character before the @</returns>
         private static bool ContainsCharacterBeforeAt(string eMailAdress)
         {
             bool result = false;
@@ -296,7 +370,11 @@ namespace FrmMain_LoginForm_PartPachler
             return (result);
         }
 
-        //Check if there is no '.' at the start, end of next to the @
+        /// <summary>
+        /// Check if there is no '.' at the start, end of next to the @
+        /// </summary>
+        /// <param name="eMailAdress"></param>
+        /// <returns>true if there are no dots in invalid positions</returns>
         private static bool NoDotsAtInvalidPosition(string eMailAdress)
         {
             bool result = true;
@@ -326,7 +404,11 @@ namespace FrmMain_LoginForm_PartPachler
             return (result);
         }
 
-        //Checks if there are no invalid symbols
+        /// <summary>
+        /// Checks if there are no invalid symbols
+        /// </summary>
+        /// <param name="eMailAdress"></param>
+        /// <returns>true if there are only valid symbols</returns>
         private static bool NoInvalidSymbols(string eMailAdress)
         {
             bool result = true;
@@ -349,7 +431,12 @@ namespace FrmMain_LoginForm_PartPachler
             return (result);
         }
 
-        //Checks if the E-Mail-Adress is already listed in the Customerlist
+        /// <summary>
+        /// Checks if the E-Mail-Adress is already listed in the Customerlist
+        /// </summary>
+        /// <param name="customerList"></param>
+        /// <param name="eMailAdress"></param>
+        /// <returns>true if the E-Mail-Adress is not listed yet</returns>
         private static bool EMailAdressNotListedYet(List<Customer> customerList, string eMailAdress)
         {
             bool result = true;
@@ -367,6 +454,10 @@ namespace FrmMain_LoginForm_PartPachler
         #endregion
 
         #region dynamic Methods
+        /// <summary>
+        /// Creates a string which contains the data of one Customer Object for Output on GUI.
+        /// </summary>
+        /// <returns>GUI outputstring</returns>
         public string ToVisualString()
         {
             string result = String.Format(
@@ -376,6 +467,11 @@ namespace FrmMain_LoginForm_PartPachler
                 );
             return (result);
         }
+
+        /// <summary>
+        /// Creates a string which contains the data of one Customer Object for storage in a .csv file.
+        /// </summary>
+        /// <returns>.csv outputstring</returns>
         public override string ToString()
         {
             string result =
@@ -388,7 +484,6 @@ namespace FrmMain_LoginForm_PartPachler
 
             return (result);
         }
-        //Pachler Martin:
         #endregion
 
     }
